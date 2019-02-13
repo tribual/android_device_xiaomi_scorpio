93a1647778ebb62dc3db600e3fc79dac974744ea
tribual <tribual@web.de>
2019-02-13 10:25:00 +0100

    make it bootleg

--- bootleg_scorpio.mk
+++ bootleg_scorpio.mk
@@ -21,11 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from scorpio device
 $(call inherit-product, device/xiaomi/scorpio/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bootleg stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_scorpio
+PRODUCT_NAME := bootleg_scorpio
 PRODUCT_DEVICE := scorpio
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI Note 2
@@ -40,3 +40,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:8.0.0/OPR1.170623.032/V9.6.1.0.OADMIFD:user/release-keys"
+
+#Maintainer
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="tribual"
+
+#Make it official
+BOOTLEGGERS_BUILD_TYPE=Shishufied
