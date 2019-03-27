087977a75199aea2aa00fb60d9108d2daf807e28
tribual <tribual@web.de>
2019-03-27 20:07:09 +0100

    scorpio: add frameworks boost

--- bootleg_scorpio.mk
+++ bootleg_scorpio.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from scorpio device
 $(call inherit-product, device/xiaomi/scorpio/device.mk)
 
+# Add Framework boost
+$(call inherit-product-if-exists, vendor/qcom/common/qti-vendor.mk)
+
 # Inherit some common Bootleg stuff.
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
